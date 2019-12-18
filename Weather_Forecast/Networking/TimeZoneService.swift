cea8a799ea9db5a404db9e016e821ae79dda194c
mbilohatniuk <m.bilohatniuk@gmail.com>
2019-12-18 16:35:21 +0200

    added UserDefaults

---
+++ Weather_Forecast/Networking/TimeZoneService.swift
@@ -0,0 +1,13 @@
+//
+//  TimeZoneService.swift
+//  Weather_Forecast
+//
+//  Created by Maksym Bilohatniuk on 12/18/19.
+//  Copyright © 2019 Maksym Bilohatniuk. All rights reserved.
+//
+
+import Foundation
+
+class TimeZoneService {
+    
+}
